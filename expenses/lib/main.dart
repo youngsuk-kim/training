9a4b233aa283817e47156054c72fa2cdf379b160
youngsuk <youngsuk-kim@github.com>
2024-12-17 07:27:14 +0900

    Initial commit

---
+++ expenses/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:expenses/expenses.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    )
+  );
+}
---
+++ expenses/lib/models/expenses.dart
@@ -0,0 +1,6 @@
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
